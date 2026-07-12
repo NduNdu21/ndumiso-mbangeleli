059780bf3c9f1637295c6c50b88b4c2fa4f3795f
Ndumiso Mbangeleli <ndumisobm@icloud.com>
2026-07-12 11:20:27 +0100

    Setting up Landing page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import Hero from "./sections/Hero";
-import Projects from "./sections/Projects";
-import Contacts from "./sections/Contacts";
-import About from "./sections/About";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import BlogList from "./pages/BlogList";
+import BlogPost from "./pages/BlogPost";
 
 function App() {
   return (
-    <>
-      <Hero />
-      <About />
-      <Projects />
-      <Contacts />
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/blog" element={<BlogList />} />
+      <Route path="/blog/:slug" element={<BlogPost />} />
+    </Routes>
+  );
 }
 
 export default App;
